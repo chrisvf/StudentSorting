63b151edb516f7651f586c2a247836c91d006702
chrisvf <chrisvfabio@gmail.com>
2014-04-29 21:17:59 +0800

    Updated Main Class.

--- src/com/chrisvfabio/app/Main.java
+++ src/com/chrisvfabio/app/Main.java
@@ -16,10 +16,61 @@
 
 package com.chrisvfabio.app;
 
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static final int MAX_STUDENTS = 20;
+    private int numElems;
+    private Student[] students;
+
+    public Main() {
+        students = new Student[MAX_STUDENTS];
+        numElems = 0;
     }
 
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.insertStudents();
+        m.displayStudents();
+        m.insertionSort();
+        m.displayStudents();
+    }
+
+    public void insertStudents() {
+        for (int i = 0; i < 10; i++) {
+
+            String firstName = "First name";
+            String lastName = "Last name";
+            double grade = Math.abs((new Random().nextDouble() * 100));
+
+            students[i] = new Student(firstName, lastName, grade);
+
+            numElems++;
+        }
+    }
+
+    public void displayStudents() {
+        System.err.println("Displaying students~");
+        for (int i = 0; i < numElems; i++) {
+            System.out.println(String.format("[%d] %.2f", i, students[i].getGrade()));
+            // System.out.println(String.format("Student[%d]\nFirst name: %s\nLast name: %s\nGrade: %.2f", i, students[i].getFirstName(), students[i].getLastName(), students[i].getGrade()));
+        }
+    }
+
+    public void insertionSort() {
+        int in, out;
+
+        for (out = 1; out < numElems; out++) {
+            Student temp = students[out];
+            in = out;
+
+            while (in > 0 && students[in - 1].getGrade() >= temp.getGrade()) {
+                students[in] = students[in - 1];
+                --in;
+            }
+
+            students[in] = temp;
+        }
+    }
 }
